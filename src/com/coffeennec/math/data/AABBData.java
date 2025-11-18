bd5de2640da913fbe48c63fba93af9f922fa9de2
JRekSahriP <jpedro95004@gmail.com>
2025-11-18 09:46:11 -0300

    Add data package

---
+++ src/com/coffeennec/math/data/AABBData.java
@@ -0,0 +1,32 @@
+package com.coffeennec.math.data;
+
+import com.coffeennec.math.Point2D;
+
+public class AABBData {
+	private final Point2D min;
+	private final Point2D max;
+	
+	public AABBData(float minX, float minY, float maxX, float maxY) {
+		this.min = new Point2D(minX, minY);
+		this.max = new Point2D(maxX, maxY);
+	}
+	public AABBData(Point2D min, Point2D max) {
+		this.min = min;
+		this.max = max;
+	}
+	
+	public boolean intersects(AABBData other) {
+		return 	this.min.x < other.max.x &&
+				this.max.x > other.min.x &&
+				this.min.y < other.max.y &&
+				this.max.y > other.min.y;
+	}
+	
+	public Point2D getMin() {
+		return min;
+	}
+	public Point2D getMax() {
+		return max;
+	}
+
+}
---
+++ src/com/coffeennec/math/data/RangeData.java
@@ -0,0 +1,19 @@
+package com.coffeennec.math.data;
+
+public class RangeData {
+	private final float min;
+	private final float max;
+	
+	public RangeData(float min, float max) {
+		this.min = min;
+		this.max = max;
+	}
+
+	public float getMin() {
+		return min;
+	}
+	public float getMax() {
+		return max;
+	}
+	
+}
