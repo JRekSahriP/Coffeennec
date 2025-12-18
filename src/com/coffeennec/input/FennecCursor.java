9551edd05e377d152348f49ccdab2c9788419fbd
JRekSahriP <cachorrodetoddy@gmail.com>
2025-12-17 21:35:28 -0300

    Refactor(input): implement support for multiple simultaneous mouse buttons

--- src/com/coffeennec/input/FennecCursor.java
+++ src/com/coffeennec/input/FennecCursor.java
@@ -4,115 +4,122 @@ import java.awt.Point;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseWheelEvent;
+import java.util.Arrays;
 
 public class FennecCursor extends MouseAdapter {
 
+	public static final int LEFT_BUTTON = 1;
+	public static final int MIDDLE_BUTTON = 2;
+	public static final int RIGHT_BUTTON = 3;
+	
+	
     private static Point MousePosition = new Point();
 	private static Point MousePositionOnScreen = new Point();
     
-    private static boolean isMousePressed = false;
     private static boolean isMouseOnScreen = false;
     
-    private static int mouseButtonPressed = -1; // -1 == nothing
-
+    private static boolean[] mouseButtons = new boolean[10];
+    private static boolean[] mouseButtonsClicked = new boolean[10];
+    private static boolean[] mouseButtonsClickedBuffer = new boolean[10];
+    
     
     private static int wheelRotation = 0;
     private static boolean isWheelMoved = false;
 
     public static void update() {
-    	isWheelMoved = false;
-    	wheelRotation = 0;
+    	FennecCursor.isWheelMoved = false;
+    	FennecCursor.wheelRotation = 0;
+    	
+    	System.arraycopy(mouseButtonsClickedBuffer, 0, mouseButtonsClicked, 0, 10);
+    	Arrays.fill(FennecCursor.mouseButtonsClickedBuffer, false);
     }
     
 	@Override
 	public void mouseWheelMoved(MouseWheelEvent e) {
-		wheelRotation = e.getWheelRotation();
-	    isWheelMoved = true;	    
+		FennecCursor.wheelRotation = e.getWheelRotation();
+	    FennecCursor.isWheelMoved = true;	    
 	}
 
 	@Override
 	public void mouseDragged(MouseEvent e) {
-		MousePosition = e.getPoint();
-		MousePositionOnScreen = e.getLocationOnScreen();	
+		FennecCursor.MousePosition = e.getPoint();
+		FennecCursor.MousePositionOnScreen = e.getLocationOnScreen();	
 	}
 
 	@Override
 	public void mouseMoved(MouseEvent e) {
-		MousePosition = e.getPoint();
-		MousePositionOnScreen = e.getLocationOnScreen();
+		FennecCursor.MousePosition = e.getPoint();
+		FennecCursor.MousePositionOnScreen = e.getLocationOnScreen();
 	}
 
 
 	@Override
 	public void mouseEntered(MouseEvent e) {
-		isMouseOnScreen = true;
+		FennecCursor.isMouseOnScreen = true;
 	}
 
 	@Override
 	public void mouseExited(MouseEvent e) {
-		isMouseOnScreen = false;
+		FennecCursor.isMouseOnScreen = false;
 	}
 
 	@Override
 	public void mousePressed(MouseEvent e) {
-		isMousePressed = true;
-		mouseButtonPressed = e.getButton();
+		int button = e.getButton();
+		
+		if (button >= FennecCursor.mouseButtons.length) {
+			return;
+		}
+		
+		FennecCursor.mouseButtons[button] = true;
+		FennecCursor.mouseButtonsClickedBuffer[button] = true;
 	}
 	
 	@Override
 	public void mouseReleased(MouseEvent e) {
-		isMousePressed = false;
-		mouseButtonPressed = -1;
+		int button = e.getButton();
+		
+		if (button >= FennecCursor.mouseButtons.length) {
+			return;
+		}
+		
+		FennecCursor.mouseButtons[button] = false;
 	}
 	
-    public static Point getMousePosition() {
-		return MousePosition;
+	public static boolean isButtonPressed(int button) {
+		return FennecCursor.mouseButtons[button];
+	}
+	
+	public static boolean isButtonClicked(int button) {
+		return FennecCursor.mouseButtonsClicked[button];
+	}
+
+	
+	public static Point getMousePosition() {
+		return FennecCursor.MousePosition;
 	}
 
 	public static Point getMousePositionOnScreen() {
-		return MousePositionOnScreen;
+		return FennecCursor.MousePositionOnScreen;
 	}
 	
 	public static boolean isMouseOnScreen() {
-		return isMouseOnScreen;
-	}
-
-	
-	
-	public static boolean isMousePressed() {
-		return isMousePressed;
-	}
-
-	public static int getMouseButtonPressed() {
-		return mouseButtonPressed;
+		return FennecCursor.isMouseOnScreen;
 	}
 	
-	public static boolean isLeftButtonPressed() {
-		return mouseButtonPressed == 1;
-	}
-	
-	public static boolean isMiddleButtonPressed() {
-		return mouseButtonPressed == 2;
-	}
-	
-	public static boolean isRightButtonPressed() {
-		return mouseButtonPressed == 3;
-	}
-	
-
 	public static int getWheelRotation() {
-		return wheelRotation;
+		return FennecCursor.wheelRotation;
 	}
 	
 	public static boolean isWheelMovedUp() {
-		return wheelRotation < 0;
+		return FennecCursor.wheelRotation < 0;
 	}
 	public static boolean isWheelMovedDown() {
-		return wheelRotation > 0;
+		return FennecCursor.wheelRotation > 0;
 	}
 	
 	public static boolean isWheelMoved() {
-		return isWheelMoved;
+		return FennecCursor.isWheelMoved;
 	}
 
 	
